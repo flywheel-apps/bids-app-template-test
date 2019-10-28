50a82c9323b056894135ec23343c3e14d5a9a4aa
Andy Worth <AndyWorth@Flywheel.io>
2019-10-28 17:43:23 -0400

    remove unnecessary print

--- run_tests.py
+++ run_tests.py
@@ -47,7 +47,6 @@ def main(test):
         f'{MANIFEST["custom"]["docker-image"]}'
 
     print('Command:\n\n'+cmd+'\n')
-    print('PYTHONUNBUFFERED is ' + os.environ.get('PYTHONUNBUFFERED','None'))
 
     command = [ w for w in cmd.split() ]
 
